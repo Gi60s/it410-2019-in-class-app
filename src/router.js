510525a6035e49734cc921bd1ad8128f2e76ab2c
james <james.speirs@gmail.com>
2019-01-24 09:09:56 -0700

    init commit

---
+++ src/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/contacts',
+      name: 'contacts',
+      component: () => import(/* webpackChunkName: "contacts" */ './views/Contacts.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "login" */ './views/Login.vue')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import(/* webpackChunkName: "profile" */ './views/Profile.vue')
+    }
+  ]
+})
---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      Username: <input type="text" v-model="username"><br>
+      Password: <input type="password" v-model="password"><br>
+      <input type="submit" value="Login">
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  data: () => {
+    return {
+      username: "",
+      password: ""
+    }
+  },
+  methods: {
+    login () {
+      window.user = {
+        username: this.username,
+        password: this.password
+      }
+      this.$router.push({ name: 'home'});
+    }
+  }
+}
+</script>
